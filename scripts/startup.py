965e5284c52d05c14ddcb7f38a91434a6808aaf5
7HAL32 <lutz.thies@tu-dresden.de>
2017-03-05 03:31:29 +0100

    updated initial Python script, added creation of src folder in home directory of robot

--- scripts/startup.py
+++ scripts/startup.py
@@ -1,4 +1,9 @@
 import ev3dev.ev3 as ev3
 import reloader
-reloader.blacklist(["ev3dev"])
-reloader.enable()
+reloader.enable(blacklist=['ev3dev.ev3', 'ev3dev', 'ev3'])
+import os
+print("ev3-robolab-startup v.1.0")
+print("Working directory is:", os.getcwd())
+print("Beginning with execution")
+print("-------------------------")
+import main
---
+++ src/main.py
@@ -0,0 +1,2 @@
+import ev3dev.ev3 as ev3
+print("Hello World!")
